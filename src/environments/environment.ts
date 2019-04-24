54199fbb66971100e93e134b2e1a3c8431ae694d
Santosh Purja Pun <punsantosh1991@outlook.com>
2019-04-24 20:03:40 +1000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAZSRlNNebGPuaprKOnxBn1DpG9OSmIvJ0",
-    authDomain: "simplenotes-4fc26.firebaseapp.com",
-    databaseURL: "https://simplenotes-4fc26.firebaseio.com",
-    projectId: "simplenotes-4fc26",
-    storageBucket: "simplenotes-4fc26.appspot.com",
-    messagingSenderId: "657318846088"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
 
