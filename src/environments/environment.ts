9b65f8165fa2a131888d51c0483948a14098030f
Santosh Purja Pun <punsantosh1991@outlook.com>
2019-04-24 21:43:26 +1000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,13 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+
 export const environment = {
   production: false,
+  
+  // replace the firebaseConfig with your own configuration values.
+  
   firebaseConfig: {
     apiKey: "",
     authDomain: "",
